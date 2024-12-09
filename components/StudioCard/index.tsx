fa66901f474d9a1fba2bd1a91757d5d68c34c972
avangardewashere <avelpanaligan@gmail.com>
2024-12-09 16:10:04 +0800

    first commit/setup

---
+++ components/StudioCard/index.tsx
@@ -0,0 +1,7 @@
+export default function StudioCard(){
+	return(
+		<div className="w-64 border-blue-200 border ">
+			StudioCard
+		</div>
+	)
+}
---
+++ components/navBar/index.tsx
@@ -0,0 +1,7 @@
+export default function NavBar(){
+	return(
+		<div className="w-48 m-auto my-4 rounded-md h-12 flex items-center justify-center bg-sky-400 text-white">
+			NavBar
+		</div>
+	)
+}
